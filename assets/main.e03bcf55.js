0667c330d4bf96c5ce7506584daf735200aedd01
Anh Nguyen <tuananh.exp@gmail.com>
2022-02-20 19:33:42 +0700

    deploy

--- assets/main.e03bcf55.js
+++ assets/main.e03bcf55.js
@@ -1,7 +1,7 @@
 var __vite_style__=document.createElement("style");__vite_style__.innerHTML=`html{font-size:62.5%}body{font-size:1.6rem;font-family:Aeonik;font-style:normal;font-weight:400;color:#060606}.container{width:100%;padding-left:15px;padding-right:15px}@media (min-width: 990px){.container{width:990px}}@media (min-width: 1200px){.container{width:1200px}}@media (min-width: 1440px){.container{width:1440px}}
-`;document.head.appendChild(__vite_style__);define(["./vendor.6da99882"],function(e){"use strict";var i="",t="/branch8-assigment/assets/favicon.17e50649.svg",n="/branch8-assigment/assets/img_point1.08e24c07.png";document.querySelector("#app").innerHTML=`
+`;document.head.appendChild(__vite_style__);define(["./vendor.6da99882"],function(e){"use strict";var n="",t="/vite-deploying-ex/assets/favicon.17e50649.svg",i="/vite-deploying-ex/assets/img_point1.08e24c07.png";document.querySelector("#app").innerHTML=`
   <h1>Hello Vite - update!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-  <img src="${n}" alt="Image example" />
+  <img src="${i}" alt="Image example" />
   <img src="${t}" alt="vite logo" />
 `,e.$("#app").append("<p>Hello jQuery - update!</p>")});
