c7c17e523205a65fca4df37f45318f2e15614885
Anh Nguyen <tuananh.exp@gmail.com>
2022-02-20 18:24:10 +0700

    ✨ deploying dist

---
+++ assets/main.5d1de54e.js
@@ -0,0 +1,6 @@
+import{$ as e}from"./vendor.173eabb8.js";var a="/vite-deploying-ex/assets/favicon.17e50649.svg",t="/vite-deploying-ex/assets/img_point1.08e24c07.png";document.querySelector("#app").innerHTML=`
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <img src="${t}" alt="Image example" />
+  <img src="${a}" alt="vite logo" />
+`;e("#app").append("<p>Hello jQuery!</p>");
