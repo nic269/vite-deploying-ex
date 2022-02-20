b5fa1c07801e4c1e5194cf588973f73e00024d2b
Anh Nguyen <tuananh.exp@gmail.com>
2022-02-20 19:30:36 +0700

    deploy

--- assets/main.cbdd2f0e.js
+++
@@ -1,6 +0,0 @@
-import{$ as e}from"./vendor.173eabb8.js";var a="/vite-deploying-ex/assets/favicon.17e50649.svg",t="/vite-deploying-ex/assets/img_point1.08e24c07.png";document.querySelector("#app").innerHTML=`
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-  <img src="${t}" alt="Image example" />
-  <img src="${a}" alt="vite logo" />
-`;e("#app").append("<p>Hello jQuery - update!</p>");
